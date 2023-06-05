d4b5619a719711b610912049f8debb6a42b42a97
O-Kareta <aleksandr.kareta@gmail.com>
2023-06-05 12:36:28 +0300

    ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onOpenImg);
+
+function createGalleryCardMarkup(cards) {
+  return cards
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onOpenImg(event) {
+  event.preventDefault();
+
+  galleryContainer.addEventListener("keydown", onEscKeyPress, { once: true });
+
+  const isGalleryContainerEl =
+    event.target.classList.contains("gallery__image");
+
+  if (!isGalleryContainerEl) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.body.querySelector(".gallery");
+galleryContainer.innerHTML = galleryItems.reduce((htmlGallery, item) => {
+  return (
+    htmlGallery +
+    `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+    </li>
+    `
+  );
+}, "");
+
+const lightbox = new SimpleLightbox(`.gallery a`, {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+console.log(galleryContainer);
